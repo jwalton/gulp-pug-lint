0973ed5bd1490ac541b8a4f4b90f59c675ea2c97
Jason Walton <github@lucid.thedreaming.org>
2017-11-01 14:18:33 -0400

    Update dependencies.

--- config_parser.js
+++ config_parser.js
@@ -5,11 +5,12 @@ var path = require('path');
 var util = require('util');
 
 module.exports = function configParser(config) {
+  var configPath, defaultConfig;
   if (config.extends) {
-    var configPath = path.resolve(util.format('node_modules/pug-lint-config-%s', config.extends), 'index.js');
+    configPath = path.resolve(util.format('node_modules/pug-lint-config-%s', config.extends), 'index.js');
 
     try {
-      var defaultConfig = require(configPath);
+      defaultConfig = require(configPath);
       return _.extend({}, defaultConfig, _.omit(config, 'extends'));
     } catch (e) {
       // no prob
--- index.js
+++ index.js
@@ -10,24 +10,28 @@ module.exports = function(options) {
   var rc = new RcLoader('.pug-lintrc', options);
 
   return through.obj(function(file, enc, cb) {
+    var linter, errors;
     if (file.isNull()) {
-      return cb(null, file);
+      cb(null, file);
+      return;
     }
 
     if (file.isStream()) {
-      return cb(new gutil.PluginError('gulp-pug-lint', 'streaming not supported'));
+      cb(new gutil.PluginError('gulp-pug-lint', 'streaming not supported'));
+      return;
     }
 
     rc.for(file.path, function(errRc, conf) {
       if (errRc) {
-        return cb(new gutil.PluginError('gulp-pug-lint', errRc));
+        cb(new gutil.PluginError('gulp-pug-lint', errRc));
+        return;
       }
 
       try {
-        var linter = new PugLint();
+        linter = new PugLint();
         linter.configure(configParser(conf));
 
-        var errors = linter.checkFile(file.path);
+        errors = linter.checkFile(file.path);
 
         if (errors.length) {
           gutil.log(gutil.colors.cyan(errors.length) + ' issues found in ' + gutil.colors.magenta(file.path));
@@ -36,7 +40,8 @@ module.exports = function(options) {
           });
         }
       } catch (errLint) {
-        return cb(new gutil.PluginError('gulp-pug-lint', errLint));
+        cb(new gutil.PluginError('gulp-pug-lint', errLint));
+        return;
       }
 
       cb(null, file);
